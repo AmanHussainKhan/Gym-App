fe4a2bc3c510ac94d3c5e8bcdc0887c7c8c7a624
Aman Hussain Khan <amanhussainkhan3@gmail.com>
2024-10-07 15:50:21 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>Developed By Aman Hussain Khan. (coding alone diaries)</footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <p>Pune Athlete's Club</p>
+    </header>
+  );
+};
+
+export default Navbar;
